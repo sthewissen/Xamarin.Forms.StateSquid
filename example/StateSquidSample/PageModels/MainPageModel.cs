ad32cd17b5997b7b1d629d533e445d28b223b0df
sthewissen <github@thewissen.io>
2019-08-05 13:38:52 +0200

    Updated sample project

--- example/StateSquidSample/PageModels/MainPageModel.cs
+++ example/StateSquidSample/PageModels/MainPageModel.cs
@@ -9,36 +9,34 @@ namespace StateSquidSample.PageModels
     public class MainPageModel : FreshMvvm.FreshBasePageModel
     {
         public State CurrentState { get; set; }
+        public State SkeletonState { get; set; }
 
-        //public bool IsFullscreenLoading { get; set; }
-        //public bool IsSkeletonLoading { get; set; }
-
-        //public ICommand FullscreenLoadingCommand { get; set; }
-        //public ICommand SkeletonCommand { get; set; }
-        //public ICommand RepeatingCommand { get; set; }
+        public ICommand FullscreenLoadingCommand { get; set; }
+        public ICommand SkeletonCommand { get; set; }
+        public ICommand RepeatingCommand { get; set; }
 
         public ICommand CycleStatesCommand { get; set; }
 
         public MainPageModel()
         {
-            //FullscreenLoadingCommand = new Command(async (x) =>
-            //{
-            //    IsFullscreenLoading = true;
-            //    await Task.Delay(2000);
-            //    IsFullscreenLoading = false;
-            //});
+            FullscreenLoadingCommand = new Command(async (x) =>
+            {
+                CurrentState = State.Loading;
+                await Task.Delay(2000);
+                CurrentState = State.None;
+            });
 
-            //SkeletonCommand = new Command(async (x) =>
-            //{
-            //    IsSkeletonLoading = true;
-            //    await Task.Delay(2000);
-            //    IsSkeletonLoading = false;
-            //});
+            SkeletonCommand = new Command(async (x) =>
+            {
+                SkeletonState = State.Loading;
+                await Task.Delay(2000);
+                SkeletonState = State.None;
+            });
 
-            //RepeatingCommand = new Command(async (x) =>
-            //{
-            //    await CoreMethods.PushPageModel<ListViewPageModel>();
-            //});
+            RepeatingCommand = new Command(async (x) =>
+            {
+                await CoreMethods.PushPageModel<ListViewPageModel>();
+            });
 
             CycleStatesCommand = new Command(async (x) => await CycleStates());
         }
